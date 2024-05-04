5b2644f8c7913156fef7585399e49f7e3e3977ee
fadegor05 <fad.egor05@gmail.com>
2024-05-04 20:47:14 +0400

    added arrow

--- src/components/greet/Greet.vue
+++ src/components/greet/Greet.vue
@@ -1,11 +1,15 @@
 <script setup>
   import Title from './Title.vue';
   import Status from './Status.vue';
+  import { ArrowDownCircleIcon } from '@heroicons/vue/20/solid';
 </script>
 
 <template>
   <div>
     <Title title="CUBESHIELD" subtitle="Уникальный проект для тебя" class="mt-52"></Title>
-    <Status content="Ееееее." class="mt-52"></Status>
+    <!--<Status content="Ееееее." class="mt-40"></Status>-->
+    <div class="flex justify-center">
+      <ArrowDownCircleIcon class="size-6 fill-zinc-50 animate-bounce mt-32"></ArrowDownCircleIcon>
+    </div>
   </div>
 </template>
--- src/components/greet/Title.vue
+++ src/components/greet/Title.vue
@@ -3,7 +3,7 @@
         <div class="text-zinc-50 font-black text-6xl text-center">
             {{ title }}
         </div>
-        <div class="from-zinc-50 to-zinc-800 bg-clip-text text-transparent bg-gradient-to-r font-bold text-4xl text-center">{{ subtitle }}</div>
+        <div class="from-purple-600 via-red-400 to-blue-600 bg-clip-text text-transparent bg-gradient-to-r font-bold text-4xl text-center animate-gradient">{{ subtitle }}</div>
     </div>
   </template>
   
